b678bc4ec7ceb3d5eec70ac4e05f8c649b3216a2
shuxinghu <beasonshu@sina.com>
2019-08-16 14:33:24 +0800

    222

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,17 +8,17 @@ Page({
     hasUserInfo: false,
     //canIUse: wx.canIUse('button.open-type.getUserInfo'),
     imgUrls: [{
-        'id': 0,
-        'img': 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2062955420,1345462341&fm=26&gp=0.jpg'
-      },
-      {
-        'id': 1,
-        'img': 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2640477548,672523077&fm=26&gp=0.jpg'
-      },
-      {
-        'id': 2,
-        'img': 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1025942878,1952470296&fm=26&gp=0.jpg'
-      }
+      'id': 0,
+      'img': 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2062955420,1345462341&fm=26&gp=0.jpg'
+    },
+    {
+      'id': 1,
+      'img': 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2640477548,672523077&fm=26&gp=0.jpg'
+    },
+    {
+      'id': 2,
+      'img': 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1025942878,1952470296&fm=26&gp=0.jpg'
+    }
     ],
     indicatorDots: false,
     autoplay: true,
@@ -27,15 +27,16 @@ Page({
     circular: true,
     loadingHidden: true,
     ic_avatar: 'https://upload.jianshu.io/users/upload_avatars/6095830/2c92a70e-0a32-4347-bc1b-419536019afc?imageMogr2/auto-orient/strip|imageView2/1/w/240/h/240',
+    dataList: { "banquet": [{ "title": "富贵皇宫", "square": "540m²", "floors": "1楼", "tables": "40桌", "thumbnail": "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2062955420,1345462341&fm=26&gp=0.jpg", "detail": "" }, { "title": "富贵皇宫", "square": "540m²", "floors": "1楼", "tables": "40桌", "thumbnail": "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2062955420,1345462341&fm=26&gp=0.jpg", "detail": "" }, { "title": "富贵皇宫", "square": "540m²", "floors": "1楼", "tables": "40桌", "thumbnail": "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2062955420,1345462341&fm=26&gp=0.jpg", "detail": "" }, { "title": "富贵皇宫", "square": "540m²", "floors": "1楼", "tables": "40桌", "thumbnail": "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2062955420,1345462341&fm=26&gp=0.jpg", "detail": "" }], "menu": [{ "name": "金玉满堂宴", "price": "10" }, { "name": "寿比南山宴", "price": "20" }, { "name": "喜结良缘宴", "price": "30" }], "banquetDescription": { "url": "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2062955420,1345462341&fm=26&gp=0.jpg", "detail": "gwegwegwegwegwegwe" } },
     for_list: [{ "name": "q" }, { "name": "2" }, { "name": "3" }]
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
-  onLoad: function() {
+  onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
@@ -63,7 +64,7 @@ Page({
       })
     }
   },
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
     // 触发下拉刷新时执行
     this.showNavigationBarLoading;
     this.setData({
@@ -73,27 +74,27 @@ Page({
       this.setData({
         loadingHidden: true,
         imgUrls: [{
-            'id': 0,
-            'img': 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1565696543045&di=c2b33467f8313c4607161284ced262e5&imgtype=0&src=http%3A%2F%2Fg.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2F838ba61ea8d3fd1f97d14a90304e251f94ca5f44.jpg'
-          },
-          {
-            'id': 1,
-            'img': 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1565696543045&di=57d4fe3e084bfab19a55da3696e4c8d0&imgtype=0&src=http%3A%2F%2Fimg.pconline.com.cn%2Fimages%2Fupload%2Fupc%2Ftx%2Fitbbs%2F1011%2F18%2Fc0%2F5904016_1290049685719_1024x1024.jpg'
-          },
-          {
-            'id': 2,
-            'img': 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1565696543045&di=6a61c2dd8698237343d1057dd98a1596&imgtype=0&src=http%3A%2F%2Fimage.xcar.com.cn%2Fattachments%2Fa%2Fday_141105%2F2014110511_fe55f356baeb917f99e9Q95QVNshO3Eo.jpg'
-          }, {
-            'id': 3,
-            'img': 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3243149245,4073544300&fm=26&gp=0.jpg'
-          }
+          'id': 0,
+          'img': 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1565696543045&di=c2b33467f8313c4607161284ced262e5&imgtype=0&src=http%3A%2F%2Fg.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2F838ba61ea8d3fd1f97d14a90304e251f94ca5f44.jpg'
+        },
+        {
+          'id': 1,
+          'img': 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1565696543045&di=57d4fe3e084bfab19a55da3696e4c8d0&imgtype=0&src=http%3A%2F%2Fimg.pconline.com.cn%2Fimages%2Fupload%2Fupc%2Ftx%2Fitbbs%2F1011%2F18%2Fc0%2F5904016_1290049685719_1024x1024.jpg'
+        },
+        {
+          'id': 2,
+          'img': 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1565696543045&di=6a61c2dd8698237343d1057dd98a1596&imgtype=0&src=http%3A%2F%2Fimage.xcar.com.cn%2Fattachments%2Fa%2Fday_141105%2F2014110511_fe55f356baeb917f99e9Q95QVNshO3Eo.jpg'
+        }, {
+          'id': 3,
+          'img': 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3243149245,4073544300&fm=26&gp=0.jpg'
+        }
 
         ]
       });
       wx.stopPullDownRefresh(); //通过方法调用刷新
     }, 1000)
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
@@ -101,14 +102,14 @@ Page({
       hasUserInfo: true
     })
   },
-  onItemClick: function(event) {
+  onItemClick: function (event) {
     wx.showToast({
       icon: 'none',
       title: event.target.dataset.postid + "fsgeg"
     })
   },
   webDetail: function () {
-    wx.navigateTo({   
+    wx.navigateTo({
       //保留当前页面，跳转到应用内的某个页面（最多打开5个页面，之后按钮就没有响应的）
       url: "/pages/web/web?url=http://www.jxgztv.com/spxw/333984.jhtml"
     })
